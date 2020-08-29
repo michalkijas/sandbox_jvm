2015361ad511eb24536d840c6708e0d1a78530b1
Bitbucket <kijasmichal@gmail.com>
2020-08-29 16:54:31 +0200

    Mat test

---
+++ src/test/java/java_common/MapTest.java
@@ -0,0 +1,19 @@
+package java_common;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://sztukakodu.pl/hashmap-vs-concurrenthashmap-czym-sie-roznia/?utm_source=jvm-bloggers.com&utm_medium=link&utm_campaign=jvm-bloggers
+ */
+class MapTest {
+
+    @Test
+    void map1() {
+        Map<Object, Object> map = new HashMap<>();
+//        map.compute()
+//        map.putIfAbsent()
+    }
+}
